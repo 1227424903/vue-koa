2170e2dbb4155546b54405e8e7513de9e1b1e83d
yangwupeng <yangwupeng@xiaomi.com>
2018-10-15 15:42:27 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store/'
+import VueLazyload from 'vue-lazyload'
+import infiniteScroll from 'vue-infinite-scroll'
+import VueCookie from 'vue-cookie'
+
+Vue.use(infiniteScroll)
+Vue.use(VueCookie)
+Vue.use(VueLazyload, {
+  // preLoad: 1.3,
+  // error: 'dist/error.png',
+  loading: '/static/images/load.gif'
+  // attempt: 1
+})
+Vue.config.productionTip = false
+
+// 不需要登陆的页面 => 白名单
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  render: h => h(App)
+})
